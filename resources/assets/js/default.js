7f3a3951f1dc4b671c7d913cbb4a3d14a6a1ffd3
Max <maximsa@inbox.lv>
2020-06-18 01:26:17 +0300

    Rearrange

--- resources/assets/js/default.js
+++ resources/assets/js/default.js
@@ -33,19 +33,18 @@ $('document').ready(function () {
 
 
     //Remove all products from basket
-    $('.btn-clear-basket').on('click', function (event) {
-       if(confirm('Are you sure you want to delete all products from basket?')){
-        $.ajax({
-            url: 'ajax/clearBasket.php',
-            success: function () {
-                $("#test-basket").load(" #test-basket");
+    $('.btn-clear-basket').on('click', function () {
+            if (confirm('Are you sure you want to delete all products from basket?')) {
+                $.ajax({
+                    url: 'ajax/clearBasket.php',
+                    success: function () {
+                        $("#test-basket").load(" #test-basket");
+                    }
+                })
             }
-        })}
-
-
         }
-
     )
+
 
 });
 
